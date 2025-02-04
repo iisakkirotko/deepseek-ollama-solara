8ca85d0fed2aea4b85c8f1dd5bac2e1df864462c
Iisakki Rotko <iisakki.rotko@gmail.com>
2025-02-04 13:48:40 +0100

    refactor: separate file for custom components

--- deepseek_ollama_solara/app.py
+++ deepseek_ollama_solara/app.py
@@ -2,13 +2,12 @@ import datetime
 import json
 import uuid
 from collections.abc import AsyncIterator
-from typing import Callable, List, cast
+from typing import List, cast
 
 import solara
 import solara.lab
 from ollama import AsyncClient, ChatResponse
 from ollama._types import ResponseError
-from reacton.ipyvuetify import use_event
 
 from .database import (
     connect_database,
@@ -16,8 +15,8 @@ from .database import (
     create_messages,
     get_chats,
     get_messages,
-    update_chat,
 )
+from .interface import ChatMessage, ChatTitle
 from .tools import tool_callables, tools
 from .types import ChatDict, Message
 
@@ -185,7 +184,7 @@ def Page():
             style={"width": "100%", "height": "50vh" if empty_chat else "100%"},
         ):
             if selected_chat.value is not None:
-                ChatTitle()
+                ChatTitle(selected_chat=selected_chat)
             with solara.lab.ChatBox():
                 if update_messages.pending:
                     solara.Text("Loading messages...")
@@ -195,28 +194,8 @@ def Page():
                     else:
                         model_name = selected_chat.value["model"].split(":")[0]
 
-                    for item in messages.value:
-                        if item["role"] == "tool":
-                            tool_result = json.loads(item["content"])
-                            with solara.Column(align="flex-start"):
-                                solara.Markdown(tool_result["message"])
-                        else:
-                            with solara.lab.ChatMessage(
-                                user=item["role"] == "user",
-                                avatar=False,
-                                name=model_name if item["role"] == "assistant" else "User",
-                                color="rgba(0,0,0, 0.06)"
-                                if item["role"] == "assistant"
-                                else "#ff991f",
-                                avatar_background_color="primary"
-                                if item["role"] == "assistant"
-                                else None,
-                                border_radius="20px",
-                            ):
-                                if item["chain_of_reason"] is not None:
-                                    with solara.Details(summary="Chain of Thought"):
-                                        solara.Markdown(item["chain_of_reason"] or "")
-                                solara.Markdown(item["content"] or "")
+                        for message in messages.value:
+                            ChatMessage(message, model_name)
             if promt_ai.pending:
                 solara.Text("I'm thinking...", style={"font-size": "1rem", "padding-left": "20px"})
                 solara.ProgressLinear()
@@ -277,87 +256,6 @@ def Layout(children=[]):
             style={"height": "calc(100dvh - 44px)", "flex": 1, "padding": "20px"},
         ):
             pass
-
-
-@solara.component
-def ChatTitle():
-    editing = solara.use_reactive(False)
-    # Keep a local copy of the selected chat that we can modify
-    # And then use to update the database
-    title = solara.use_reactive(selected_chat.value["title"])
-
-    def start_editing(*_ignore):
-        editing.set(True)
-
-    def cancel_editing(*_ignore):
-        editing.set(False)
-
-    async def _save_title(*_ignore):
-        cancel_editing()
-        await update_chat(selected_chat.value["id"], title.value)
-        new_details: ChatDict = {
-            "id": selected_chat.value["id"],
-            "title": title.value,
-            "model": selected_chat.value["model"],
-        }
-        selected_chat.value = new_details
-
-    save_title = solara.lab.use_task(_save_title, dependencies=None)
-
-    with solara.Div(
-        style={
-            "display": "flex",
-            "justify-content": "flex-start",
-            "flex-shrink": "0",
-            "max-width": "600px",
-            "overflow": "hidden",
-        }
-    ):
-        if editing.value:
-            solara.InputText(label="Chat Title", value=title)
-            IconButton("check", on_click=lambda: save_title())
-            IconButton("cancel", on_click=cancel_editing)
-
-        else:
-            H3(
-                children=[title.value],
-                on_click=start_editing,
-                style={"cursor": "pointer", "flex": 1},
-            )
-
-
-@solara.component
-def IconButton(icon: str, on_click: Callable[[], None] | None = None):
-    def _on_click(*_):
-        if on_click is not None:
-            on_click()
-
-    with solara.v.Btn(icon=True) as button:
-        solara.v.Icon(children=[icon])
-
-    use_event(button, "click", _on_click)
-
-
-@solara.component
-def H3(children=[], on_click=None, style: dict[str, str] | str = {}):
-    _style = solara.util._flatten_style(style)
-    with solara.v.Html(tag="h3", children=children, style_=_style) as title_el:
-        pass
-
-    def add_click_handler():
-        def _on_click(*_):
-            if on_click is not None:
-                on_click()
-
-        widget = solara.get_widget(title_el)
-        widget.on_event("click", _on_click)
-
-        def cleanup():
-            widget.on_event("click", _on_click, remove=True)
-
-        return cleanup
-
-    solara.use_effect(add_click_handler, [])
 
 
 @solara.component
